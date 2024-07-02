ee96ca1629c56c8fce297a21d6b668d6a5e90430
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-03 00:17:41 +0200

    refactoring: DB connection properties - db name, namingStrategy,synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './features/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PluralNamingStrategy } from './common/strategies/plural.naming.strategy';
+import { QuizModule } from './features/quiz/quiz.module';
+import { TestingModule } from './features/testing/testing.module';
 
 const typeOrmModule = TypeOrmModule.forRoot({
   type: 'postgres',
@@ -10,13 +13,14 @@ const typeOrmModule = TypeOrmModule.forRoot({
   port: 5432,
   username: 'postgres',
   password: 'sa',
-  database: 'bloggers_6',
+  database: 'bloggers_7',
+  namingStrategy: new PluralNamingStrategy(),
   logging: 'all',
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: false,
 });
 
-const appModules = [UsersModule];
+const appModules = [UsersModule, QuizModule, TestingModule];
 
 @Module({
   imports: [typeOrmModule, ...appModules],
