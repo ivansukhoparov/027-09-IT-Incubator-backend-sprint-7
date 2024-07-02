4cc89970f8a2419b9d0c377cee7f8152706dd54e
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-03 00:15:55 +0200

    create: add quiz questions admin flows

---
+++ src/features/quiz/questions/use.cases/delete.question.use.case.ts
@@ -0,0 +1,23 @@
+import { QuizQuestionCreateInputModel } from '../api/admin/models/quiz.question.create.input.model';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { QuizQuestionRepository } from '../infrastructure/quiz.question.repository';
+import { InterlayerNotice } from '../../../../base/models/interlayer.notice';
+import { CreateQuestionDto, UpdateQuestionDto } from '../types/input';
+import { QuizQuestionUpdateInputModel } from '../api/admin/models/quiz.question.update.input.model';
+
+export class DeleteQuizQuestionCommand {
+  public id: string;
+
+  constructor(questionId: string) {
+    this.id = questionId;
+  }
+}
+
+@CommandHandler(DeleteQuizQuestionCommand)
+export class DeleteQuizQuestionUseCase implements ICommandHandler<DeleteQuizQuestionCommand> {
+  constructor(protected readonly quizRepository: QuizQuestionRepository) {}
+
+  async execute(command: DeleteQuizQuestionCommand): Promise<InterlayerNotice<boolean>> {
+    return await this.quizRepository.deleteQuestion(command.id);
+  }
+}
