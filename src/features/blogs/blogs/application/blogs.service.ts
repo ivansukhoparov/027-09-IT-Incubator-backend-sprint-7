6ae494bd4202b7e33f10af9a9f644a47234a8e5c
ivansukhoparov <rbcdaaa@gmail.com>
2024-07-07 19:53:23 +0200

    refactoring: fix methods names in blogs service

--- src/features/blogs/blogs/application/blogs.service.ts
+++ src/features/blogs/blogs/application/blogs.service.ts
@@ -1,42 +1,38 @@
 import { BlogsRepository } from '../infrastructure/blogs.repository';
 import { Injectable } from '@nestjs/common';
-import { Blog } from '../infrastructure/blogs.schema';
-import { blogMapper } from '../types/mapper';
 import { CreateBlogInputModel, UpdateBlogInputModel } from '../api/admin/models/blogs.input.models';
+import { BlogCreateDto } from '../types/input';
 
 @Injectable()
 export class BlogsService {
   constructor(protected blogsRepository: BlogsRepository) {}
 
   async getBlogById(id: string) {
-    return this.blogsRepository.getBlogById(id);
+    return this.blogsRepository.getById(id);
   }
 
   async isBLogExist(id: string) {
-    return await this.blogsRepository.isBlogExist(id);
+    return await this.blogsRepository.isExist(id);
   }
 
   async createNewBlog(inputModel: CreateBlogInputModel) {
-    const createdAt = new Date();
-
-    const newBlogData: Blog = {
+    const blogCreateDto: BlogCreateDto = {
       name: inputModel.name,
       description: inputModel.description,
       websiteUrl: inputModel.websiteUrl,
-      createdAt: createdAt.toISOString(),
       isMembership: false,
     };
 
-    const newBlogId = await this.blogsRepository.createBlog(newBlogData);
-    const newBlog = await this.blogsRepository.getBlogById(newBlogId);
-    return blogMapper(newBlog);
+    const newBlogId = await this.blogsRepository.create(blogCreateDto);
+    const newBlog = await this.blogsRepository.getById(newBlogId);
+    return newBlog;
   }
 
   async updateBlog(blogId: string, blogUpdateDto: UpdateBlogInputModel) {
-    const result = await this.blogsRepository.updateBlog(blogId, blogUpdateDto);
+    return await this.blogsRepository.update(blogId, blogUpdateDto);
   }
 
   async deleteBlog(blogId: string) {
-    await this.blogsRepository.deleteBlog(blogId);
+    await this.blogsRepository.delete(blogId);
   }
 }
